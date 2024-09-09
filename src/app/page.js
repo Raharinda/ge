285fea848cef3f327ab3c1804b1c26d89c949e59
Raharinda <raharindanadhif@gmail.com>
2024-09-09 11:57:13 +0700

    two

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,34 @@
 "use client";
 
-import { useState } from 'react';
+import { useState, useEffect, useRef } from "react";
 import Image from "next/image";
 import svg from "../../public/icon.svg";
-import img from "../../public/bg.jpg";
+import img from "../image/hero.jpg";
 
 export default function Home() {
   const [isThamActive, setIsThamActive] = useState(false);
+  const navRef = useRef(null);
 
   const toggleTham = () => {
     setIsThamActive(!isThamActive);
   };
+
+  const handleClickOutside = (event) => {
+    if (navRef.current && !navRef.current.contains(event.target)) {
+      setIsThamActive(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isThamActive) {
+      document.addEventListener("click", handleClickOutside);
+    } else {
+      document.removeEventListener("click", handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [isThamActive]);
 
   return (
     <main>
@@ -21,28 +39,40 @@ export default function Home() {
         </span>
       </section>
 
-      <section className="flex text-sm font-medium px-4 sm:px-12 py-5 justify-between">
+      <section className="flex text-sm font-medium px-4 sm:px-12 py-6 justify-between">
         <div className="flex flex-wrap">
           <img src="icon.svg" alt="An SVG of an eye" className="size-16" />
         </div>
 
-        <div className="hidden sm:flex flex-col sm:flex-row gap-4 sm:gap-8 items-center text-center sm:text-left mt-4 sm:mt-0"></div>
-
         <div className="hidden sm:flex flex-col mt-4 sm:flex-row gap-4 sm:gap-8 text-center sm:text-left">
-          <a href="" className="hover:underline hover:transition-all hover:duration-300">
+          <a
+            href="#"
+            className="hover:underline hover:transition-all hover:duration-300"
+          >
             Tentang Kami
           </a>
-          <a href="" className="hover:underline hover:transition-all hover:duration-300">
+          <a
+            href="#"
+            className="hover:underline hover:transition-all hover:duration-300"
+          >
             Program
           </a>
-          <a href="" className="hover:underline hover:transition-all hover:duration-300">
+          <a
+            href="#"
+            className="hover:underline hover:transition-all hover:duration-300"
+          >
             Kontak
           </a>
         </div>
 
         {/* Hamburger Menu Button */}
         <div className="sm:hidden mt-4">
-          <div className={`tham tham-e-squeeze tham-w-6 ${isThamActive ? 'tham-active' : ''}`} onClick={toggleTham}>
+          <div
+            className={`tham tham-e-squeeze tham-w-6 ${
+              isThamActive ? "tham-active" : ""
+            }`}
+            onClick={toggleTham}
+          >
             <div className="tham-box">
               <div className="tham-inner" />
             </div>
@@ -52,32 +82,51 @@ export default function Home() {
 
       {/* Hamburger Menu Content */}
       <section
+        ref={navRef}
         className={`sm:hidden fixed inset-y-0 right-0 w-64 bg-black transform transition-transform duration-300 ease-in-out z-50 ${
-          isThamActive ? 'translate-x-0' : 'translate-x-full'
-        }`}
+          isThamActive ? "translate-x-0" : "translate-x-full"
+        } p-6`}
       >
-        <div className="flex flex-col p-6 space-y-4 text-white">
-          <a href="" onClick={toggleTham} className="hover:underline hover:text-white">
-            Tentang Kami
+        <div className="flex flex-col space-y-4 text-white">
+          <a
+            href="#"
+            className="hover:underline hover:text-white"
+            onClick={() => setIsThamActive(false)}
+          >
+            Tentang kami
           </a>
-          <a href="" onClick={toggleTham} className="hover:underline hover:text-white">
+          <a
+            href="#"
+            className="hover:underline hover:text-white"
+            onClick={() => setIsThamActive(false)}
+          >
             Program
           </a>
-          <a href="" onClick={toggleTham} className="hover:underline hover:text-white">
+          <a
+            href="#"
+            className="hover:underline hover:text-white"
+            onClick={() => setIsThamActive(false)}
+          >
             Kontak
           </a>
         </div>
       </section>
 
-      <section id="home" className="mt-16 sm:mt-32 px-4">
-        <div className="container mx-auto text-center">
+      <section id="" className="sm:mt-10 px-4">
+        <div className="container mx-auto text-center ">
+          <img
+            src="/icon.svg"
+            alt="An SVG of an eye"
+            className="size-36 my-10 mx-auto"
+          />
+
           <h1 className="text-3xl font-bold uppercase">Gemapala</h1>
           <p className="max-w-xl m-4 mx-auto">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
-            risus eget laoreet mollis. Pellentesque placerat magna eget mi.
+            Gemapala merupakan singkatan dari Gema Muda Ganesha Pecinta Alam
+            yang didirikan pada tahun 1981 oleh AKBP Tri Widianto
           </p>
           <button className="mt-6 px-8 py-3 rounded-full border border-black mx-auto transition hover:text-white hover:bg-black">
-            <a href="#home" className="text-md">
+            <a href="#about" className="text-md">
               Selengkapnya
             </a>
           </button>
@@ -90,21 +139,24 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="sm:px-10 mx-20">
+      <section id="about" className="sm:px-10 mx-20">
         <div>
           <h1 className="font-bold mb-12 sm:mb-24 text-3xl sm:text-5xl">
             Prologue
           </h1>
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-32 text-sm sm:text-xm">
-            <div className="flex flex-col justify-between">
-              <p className="mb-20">
-                These constructs known as "Colossi" are formed of both ancient
-                and advanced technology, but their origin cannot be verified.
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-32 mb-32 text-sm sm:text-xm text-justify">
+            <div className="flex flex-col justify-between ">
+              <p className="mb-12">
+                Gemapala merupakan salah satu Organisasi siswa tingkat SMA yang
+                berpusat di SMAN 1 Purworejo yang berdiri sejak 12 Desember
+                1981. Gemapala merupakan wadah bagi siswa SMAN 1 Purworejo untuk
+                berkegiatan di alam bebas, berkontribusi bagi masyarakat, serta
+                peduli terhadap pelestarian lingkungan.
               </p>
               <p>
-                A distinctly unique race, who could engage in psychic
-                communication with Colossi, once lived a life of peace in a land
-                named Eraveil. These beings were known as the Caelestites.
+                Gemapala berdiri di Pangenjurutengah, Purworejo. Nama yang
+                digunakan waktu itu adalah Mapala Prajnaparamita. Prajnaparamita
+                diambil....
               </p>
             </div>
 
@@ -117,27 +169,18 @@ export default function Home() {
                 A distinctly unique race, who could engage in psychic
                 communication with Colossi, once lived a life of peace in a land
                 named Eraveil. These beings were known as the Caelestites.
-              </p>
-            </div>
-          </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-32 text-sm sm:text-xm mt-16">
-            <div className="flex flex-col justify-between">
-              <p className="">
-                A distinctly unique race, who could engage in psychic
-                communication with Colossi, once lived a life of peace in a land
-                named Eraveil. These beings were known as the Caelestites.
-              </p>
-            </div>
-
-            <div className="flex flex-col justify-between">
-              <p className="mb-20">
-                These constructs known as "Colossi" are formed of both ancient
-                and advanced technology, but their origin cannot be verified.
               </p>
             </div>
           </div>
         </div>
       </section>
+
+      <section>
+        <div><h1 className="font-bold text-3xl">Overview</h1></div>
+      </section>
+
+
+
     </main>
   );
 }
